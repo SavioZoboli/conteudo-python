5cfdea315f89f64acadab9e883f3870216ae2cec
SAVIO EDUARDO ZOBOLI <savio_zoboli@estudante.sc.senai.br>
2025-10-02 16:48:05 -0300

    conteudo sobre print e variaveis

---
+++ aula1_variaveis.py
@@ -0,0 +1,12 @@
+#Variaveis são espaços na memória que servem para armazenar valores
+
+# Podemos definir variáveis com o nome dela, seguido pelo
+# sinal de igual, que significa "atribuir" e depois pelo valor.
+# nome = "Sávio" <- Crie uma variavel nome que recebe o valor "Sávio"
+
+nome = "Savio"
+salario = 1450.00
+dias_trabalhados = 22
+print("O funcionário é",nome,"(tipo: ",type(nome),")")
+print("Ele trabalhou",dias_trabalhados,"dias","(tipo: ",type(dias_trabalhados),")")
+print("O salário é",salario,"(tipo: ",type(salario),")")
